7efbb65186d82dfef4ca0348fa61d4e6304a1f76
Flakzon <flakzon@yahoo.com>
2025-03-12 23:41:53 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function updatePrice(service, rate) {
-    let quantity = document.getElementById(service + "Qty").value;
-    let total = quantity * rate;
-    document.getElementById(service + "Price").innerText = total;
-}
