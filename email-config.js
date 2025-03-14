0d5161a99bd4731548df0170195ae560c70315c1
Flakzon <flakzon@yahoo.com>
2025-03-14 15:05:50 +0530

    Delete email-config.js

--- email-config.js
+++
@@ -1,4 +0,0 @@
-const EMAILJS_USER_ID = "X3G3L3PaqkvV94-o-";
-const EMAILJS_SERVICE_ID = "service_flakzon";
-const EMAILJS_TEMPLATE_ID = "template_flakzon";
-const TO_EMAIL = "flakzon@yahoo.com"; // Aapka email yahan set hai
